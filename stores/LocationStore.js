957dee13e4726032869cbf847679c3d923b2c964
Benjamin M. Brown <benjaminbb@gmail.com>
2015-07-27 13:07:35 -0500

    adding fetch locations capabilities in Store

--- stores/LocationStore.js
+++ stores/LocationStore.js
@@ -4,14 +4,28 @@ var LocationActions = require('../actions/LocationActions');
 class LocationStore{
 	constructor(){
 		this.locations = [];
+		this.errorMessage = null;
 		this.bindListeners({
-			handleUpdateLocations: LocationActions.UPDATE_LOCATIONS
+			handleUpdateLocations: LocationActions.UPDATE_LOCATIONS,
+			handleFetchLocations: LocationActions.FETCH_LOCATIONS,
+			handleLocationsFailed: LocationsActions.LOCATIONS_FAILED
 		});
 	}
 
 	handleUpdateLocations(locations){
 		this.locations = locations;
+		this.errorMessage = null;
 	}
+
+	handleFetchLocations(){
+		//reset the array 
+		this.locations = [];
+	}
+
+	handleLocationsFailed(errorMessage){
+		this.errorMessage = errorMessage;
+	}
+
 }
 
 module.exports = alt.createStore(LocationStore, 'LocationStore');
