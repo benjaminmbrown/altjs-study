7f256811000dcc39829180acf415d7a8501e3c6b
Benjamin M. Brown <benjaminbb@gmail.com>
2015-07-27 12:35:16 -0500

    setting up folder structure and first Flux dispatcher

---
+++ alt.js
@@ -0,0 +1,6 @@
+//Creating an instance of alt, this instantiates a Flux dispatcher  & gives you methods to create your actions and stores. 
+
+var Alt = require('alt');
+var alt = new Alt();
+
+module.exports = alt;
